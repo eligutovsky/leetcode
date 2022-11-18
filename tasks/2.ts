6c05c9559fd6c5edcdd7a2a689c6a56b90d3500f
Eli Gutovsky <elig@elig-mbp.lan>
2022-11-18 14:02:12 +0200

    add 2

---
+++ tasks/2.ts
@@ -0,0 +1,34 @@
+import ListNode from "../typings/ListNode";
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  const head = new ListNode();
+  let p1 = 0;
+  let p2 = 0;
+  let carry = 0;
+  let current = head;
+  while (l1 || l2) {
+    if (l1) {
+      p1 = l1.val;
+      l1 = l1.next;
+    } else {
+      p1 = 0;
+    }
+    if (l2) {
+      p2 = l2.val;
+      l2 = l2.next;
+    } else {
+      p2 = 0;
+    }
+    const sum = p1 + p2 + carry;
+    carry = sum > 9 ? 1 : 0;
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+  }
+  if (carry) {
+    current.next = new ListNode(carry);
+  }
+  return head.next;
+}
