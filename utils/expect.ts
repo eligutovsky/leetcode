471290fb6c3865e30d112c775fcf7f609332d74d
Eli Gutovsky <github@gutovsky.me>
2023-02-12 14:05:38 +0200

    MAIN: add colors to log

--- utils/expect.ts
+++ utils/expect.ts
@@ -1,6 +1,17 @@
+enum Colors {
+    Red = '\x1b[31m',
+    Green = '\x1b[32m',
+    Yellow = '\x1b[33m',
+    Blue = '\x1b[34m',
+}
+
+const colorize = (text: string, color: Colors) => {
+    return `${color}${text}\x1b[0m`;
+}
+
 export const expect = (arg1: any, arg2: any) => {
     if (arg1 !== arg2) {
-        throw new Error(`Actual value is ${arg1}, but expected is ${arg2}`);
+        console.error(colorize(`Failed. Actual: ${arg1}, Expected: ${arg2}`, Colors.Red));
     }
-    console.info(`Passed. Actual: ${arg1}, Expected: ${arg2}`);
+    console.info(colorize(`Passed. Actual: ${arg1}, Expected: ${arg2}`, Colors.Green));
 }
