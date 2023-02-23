626e53d4a31289c296f5d16ecfeea13420cc7864
Eli Gutovsky <github@gutovsky.me>
2023-02-23 11:22:40 +0200

    main: add print

--- typings/ListNode.ts
+++ typings/ListNode.ts
@@ -1,8 +1,17 @@
 export default class ListNode {
-  val: number
-  next: ListNode | null
+  val: number;
+  next: ListNode | null;
   constructor(val?: number, next?: ListNode | null) {
-      this.val = (val===undefined ? 0 : val)
-      this.next = (next===undefined ? null : next)
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+  print() {
+    let result = `${this.val}`;
+    let node: ListNode | null = this.next;
+    while (node) {
+      result = result + "," + node.val;
+      node = node.next;
+    }
+    return result;
   }
 }
