9fffd0e596f15408e9fe14e99e24001047aa2c5a
Eli Gutovsky <github@gutovsky.me>
2023-02-23 15:02:13 +0200

    main: add equal

--- utils/expect.ts
+++ utils/expect.ts
@@ -1,20 +1,32 @@
+import _ from "lodash";
+
 enum Colors {
-    Red = '\x1b[31m',
-    Green = '\x1b[32m',
-    Yellow = '\x1b[33m',
-    Blue = '\x1b[34m',
+  Red = "\x1b[31m",
+  Green = "\x1b[32m",
+  Yellow = "\x1b[33m",
+  Blue = "\x1b[34m",
 }
 
 const colorize = (text: string, color: Colors) => {
-    return `${color}${text}\x1b[0m`;
-}
-let callCounter = 0
+  return `${color}${text}\x1b[0m`;
+};
+let callCounter = 0;
 
 export const expect = (arg1: any, arg2: any) => {
-    callCounter++
-    if (arg1 !== arg2) {
-        console.error(colorize(`Failed ${callCounter}. Actual: ${arg1}, Expected: ${arg2}`, Colors.Red));
-    } else {
-        console.info(colorize(`Passed ${callCounter}. Actual: ${arg1}, Expected: ${arg2}`, Colors.Green));
-    }
-}
+  callCounter++;
+  if (_.isEqual(arg1, arg2)) {
+    console.info(
+      colorize(
+        `Passed ${callCounter}. Actual: ${arg1} Expected: ${arg2}`,
+        Colors.Green
+      )
+    );
+  } else {
+    console.error(
+      colorize(
+        `Failed ${callCounter}. Actual: ${arg1} Expected: ${arg2}`,
+        Colors.Red
+      )
+    );
+  }
+};
